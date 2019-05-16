450c8ba1a3c2a7488c8103c670d6c0673c16ba2b
Marie-Line Ferron <ferron.ml@gmail.com>
2019-05-16 16:10:07 +0200

    connexion base de donnée finalisée avec try & catch

--- app/Kernel/DB.php
+++ app/Kernel/DB.php
@@ -6,5 +6,24 @@ namespace App\Kernel;
 
 class DB
 {
+    private $pdo;
 
+    public function __construct()
+    {
+        try{
+            $dsn = "mysql:host=localhost;dbname=td_php_db"; // définition de la base de données sur laquelle on travaille et son emplacement
+            $options = [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+
+            $this->pdo = new \PDO($dsn, "root", "root", $options);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            echo "Problème de connexion à la DB".$e->getMessage(); // affichage de l'exception avec getMessage()
+        }
+
+    }
+
+    // récupérer la connexion la base de donnée
+    public function getPdo() {
+    return $this->pdo;
+    }
 }
